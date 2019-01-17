c560710a24f07b7f08734785309cc34b9c473ebf
廖伟 <24591106@qq.com>
2019-01-17 17:15:49 +0800

    修改代码

--- src/main/java/com/example/bootserver/controller/HelloWorld.java
+++ src/main/java/com/example/bootserver/controller/HelloWorld.java
@@ -1,12 +1,20 @@
 package com.example.bootserver.controller;
 
 import com.example.bootserver.config.Config;
+import lombok.Data;
+import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
+
 @RestController
+@Slf4j
+@Data
 public class HelloWorld {
 
     /*
@@ -25,9 +33,9 @@ public class HelloWorld {
     }
 
     @RequestMapping("/")
-    public String helloWorld() {
+    public String helloWorld(@NonNull String name) {
         // return "[" + env + "]" + hello + "," + world;
-
+        log.debug("This is a debug message!");
         return config.getHello() + "," + config.getWorld();
     }
 }
