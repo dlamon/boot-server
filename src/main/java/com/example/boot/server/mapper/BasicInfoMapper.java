615b260eb6562145c37b60e26ae703fce084eef6
廖伟 <24591106@qq.com>
2019-01-23 23:13:59 +0800

    添加mapper注解

--- src/main/java/com/example/boot/server/mapper/BasicInfoMapper.java
+++ src/main/java/com/example/boot/server/mapper/BasicInfoMapper.java
@@ -2,6 +2,8 @@ package com.example.boot.server.mapper;
 
 import com.example.boot.server.model.BasicInfo;
 import com.example.boot.server.util.MybatisMapper;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BasicInfoMapper extends MybatisMapper<BasicInfo> {
 }
